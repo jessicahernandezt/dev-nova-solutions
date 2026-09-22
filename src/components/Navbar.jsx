579d3992b1293b76117ed34e25dce513bc3942aa
jessicahernandezt <jessica.hernandez@upotosina.edu.mx>
2026-09-21 19:53:23 -0600

    Merge branch 'main' of https://github.com/jessicahernandezt/dev-nova-solutions

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import {AppBar, Box, Button, Toolbar, Typography} from '@mui/material'
+
+
+
+
+
+function Navbar(){
+
+    return(
+        <>
+        <h2>Dev nova solutions navbar</h2>
+        </>
+    )
+}
+
+export default Navbar
